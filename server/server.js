89987cf98a01dfc50e6866e39bc515e14faeef96
David <davidtimilehin739@gmail.com>
2024-03-09 14:59:53 +0100

    update server

--- server/server.js
+++ server/server.js
@@ -4,7 +4,13 @@ const http = require("http");
 const { Server } = require("socket.io"); // Use uppercase Server for consistency
 const cors = require("cors");
 
-app.use(cors());
+const corsOptions ={
+  origin:'http://localhost:5173', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
 
 const server = http.createServer(app);
 
