f5ede6295c1abf9ee78c146b65900ce645bcb0ec
David <davidtimilehin739@gmail.com>
2024-03-10 07:56:53 +0100

    remove cors options

--- server/server.js
+++ server/server.js
@@ -4,11 +4,6 @@ const http = require("http");
 const { Server } = require("socket.io"); // Use uppercase Server for consistency
 const cors = require("cors");
 
-const corsOptions ={
-  origin:'http://localhost:5173', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200
-}
 app.use(cors(corsOptions));
 
 
